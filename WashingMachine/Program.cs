33d050278685cf96d306641c5158fe4cf25040c8
H8946 <H8946@TIET-327-WS-15>
2017-02-01 15:09:52 +0200

    Washer done

---
+++ WashingMachine/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WashingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Washer washer = new Washer("Samsung", false, false, 0, true);
+            int input;
+            int value;
+            do
+            {                               
+                Console.WriteLine("1. Change the state of washer");
+                Console.WriteLine("2. Change the state of sling");
+                Console.WriteLine("3. Change the water temperature(integer only)");
+                Console.WriteLine("4. Is the washer empty or not?");
+                Console.WriteLine("5. Print data");
+                Console.WriteLine("Give your choice: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    washer.Power();
+                    Console.WriteLine("State have been changed.");
+                }
+                if (input == 2)
+                {
+                    washer.Slinger();
+                    Console.WriteLine("State have been changed.");
+                }
+                if (input == 3)
+                {
+                    Console.WriteLine("Give new temperature: ");
+                    value = int.Parse(Console.ReadLine());
+                    washer.WaterTemp = value;
+                }
+                if (input == 4)
+                {
+                    washer.EmptyC();
+                    Console.WriteLine("State have been changed.");
+                }
+                if (input == 5)
+                {
+                    washer.PrintData();
+                }
+            }
+            while (input != 0);
+        }
+    }
+}
---
+++ WashingMachine/Washer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WashingMachine
+{
+    class Washer
+    {
+        public string Model { get; set; }
+        public bool IsOn { get; set; }
+        public bool Sling { get; set; }
+        public int WaterTemp { get; set; }
+        public bool Empty { get; set; }
+
+        public Washer(string model, bool ison, bool sling, int watertemp, bool empty)
+        {
+            Model = model;
+            IsOn = ison;
+            Sling = sling;
+            WaterTemp = watertemp;
+            Empty = empty;
+        }
+         public void Power()
+        {
+            if (IsOn == true)
+            {
+                IsOn = false;
+            }
+            else IsOn = true;
+        }
+        public void Slinger()
+        {
+            if(Sling == true)
+            {
+                Sling = false;
+            }
+            else Sling = true;
+        }
+        public void EmptyC()
+        {
+            if (Empty == true)
+            {
+                Empty = false;
+            }
+            else Empty = true;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Power: " + IsOn);
+            Console.WriteLine("Slinger: " + Sling);
+            Console.WriteLine("Water temp: " + WaterTemp);
+            Console.WriteLine("Empty: " + Empty);
+        }
+    }
+}
