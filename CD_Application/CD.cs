92c0d28cc1b5ccf01e5537c3add9346f5b461521
Niko <Niko@DESKTOP-CER7BEB>
2017-02-12 00:42:06 +0200

    nopee eka teht

---
+++ CD_Application/CD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_Application
+{
+    class CD
+    {
+        // Toteuta CD-luokka, joka sisältää CD:lle yleisesti kuuluvia ominaisuuksia: artisti, hinta ja biisit (ei saa olla rajattu eli käytä apuna jotain tietorakennetta).
+        public string Artist { get; set; }
+        public int Cost { get; set; }
+        public List<Song> Songs;
+    }
+}
---
+++ CD_Application/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_Application
+{
+    class Song
+    {
+        public string Name { get; set; }
+        public double Lenght { get; set; }
+    }
+}
